7a0a6d789a221012cdbad65e5a8839964a280fc5
anamulhaque22 <anamulah535367@gmail.com>
2022-07-22 00:45:17 +0600

    booking api

--- index.js
+++ index.js
@@ -5,28 +5,53 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
-// app.use(express.json());
+app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Password}@doctorsportalcluster.cymwy.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db("doctors_portal").collection("services");
-        app.get('/service',async(req, res) => {
+        const bookingCollection = client.db("doctors_portal").collection("bookings");
+        app.get('/service', async (req, res) => {
             const query = {};
-            const cursor= serviceCollection.find(query);
+            const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
         })
+
+        /* 
+            API naming convention:
+            app.get('/booking') //get all bookings in this collection or get more then one or by filer
+            app.get('/booking:id') // get a specific booking by id
+            app.post('/booking') // add a new booking
+            app.patch('/booking/:id') // update existing booking data
+            app.delete('/booking/:id') // delete existing specific booking data
+        */
+
+        //post booking
+        app.post('/booking', async (req, res) => {
+            const bookingData = req.body;
+            const query = {name: bookingData.name, date: bookingData.date, userName: bookingData.userName};
+            const exists = await bookingCollection.findOne(query);
+            if(exists){
+                return res.send({success:false, booking:exists});
+            }
+            const result = await bookingCollection.insertOne(bookingData);
+            return res.send({success:true, booking:result});
+        })
+
+
+
+
     } finally {
         // Ensures that the client will close when you finish/error
         //await client.close();
     }
 }
 run().catch(console.dir);
-console.log("urk",uri)
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Hello, From Doctors Portal');
 })
 
 app.listen(port, () => {
